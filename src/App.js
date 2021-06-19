f09c0f7dd936929e251dff9d6472392cb8cf53cb
yusufkhan004 <khanyusuf004@gmail.com>
2021-06-19 20:08:32 +0530

    created  react app and make home,section and header component;

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import Header from './components/Header';
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Section.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Section = () =>{
+    return(
+        <Wrap>
+
+        </Wrap>
+    )
+}
+
+
+export default Section
+
+const Wrap = styled.div `
+    width: 100vw; //Full viewWidth
+    height: 100vh; //vertical height
+    background-size: cover;
+    background-position: center; 
+    background: orange;
+`
